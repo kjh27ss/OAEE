bfd13494eeb0b72ad4008ce63bd961b2548fa2a3
kjh27ss <kjh27ss@naver.com>
2026-01-16 15:29:10 +0900

    mj: 이미지 수정 및 header 작업

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import logo from '../../assets/images/logo.png';
+import cart from '../../assets/images/cart.png';
+import NavBar from '../layout/NavBar';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <div className="inner d-flex">
+        <a href="#" className='tlogo'><img src={logo} alt="로고" /></a>
+        <NavBar />
+        <div className='d-flex'>
+          <a href="#"><img src={cart} alt="장바구니" /></a>
+          <button className='login-btn'>
+            LOGIN
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
